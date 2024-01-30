d2e3cb4f6411205422556c9bdec35a5073ca94c3
Patrick White <patrickwhite.swe@gmail.com>
2024-01-30 10:35:36 -0700

    fixed hydration error in mobileSidebar, fixed typo in movilesidebar

--- components/Mobile-sidebar.tsx
+++ components/Mobile-sidebar.tsx
@@ -4,8 +4,20 @@ import { Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "@/components/Sidebar";
+import { useEffect, useState } from "react";
 
 const MovileSidebar = () => {
+
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if(!isMounted) {
+    return null;
+  }
+  
   return (
     <Sheet>
       <SheetTrigger>
