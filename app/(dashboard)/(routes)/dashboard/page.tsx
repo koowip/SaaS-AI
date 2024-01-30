2b838793c93f46ccee7ffa7d0ca8210b90590118
Patrick White <patrickwhite.swe@gmail.com>
2024-01-30 12:38:09 -0700

    Heading component added

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -53,7 +53,7 @@ const Page = () => {
     <div>
       <div className="mb-8 space-y-4">
         <h2 className="text-2xl md:text-4xl font-bold text-center">Explor the power of AI</h2>
-        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">Chat with the smartest AI, create together.</p>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">Chat with AI, create together.</p>
       </div>
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
         {tools.map((tool) => (
